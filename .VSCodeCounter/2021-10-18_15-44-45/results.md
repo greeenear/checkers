eeda0f9093f228679aff463da59e42513d41f3ce
yra esepenok <113322greenear@gmail.com>
2021-10-18 18:19:11 +0300

    refactoring

---
+++ .VSCodeCounter/2021-10-18_15-44-45/results.md
@@ -0,0 +1,35 @@
+# Summary
+
+Date : 2021-10-18 15:44:45
+
+Directory d:\proj\git\checkers
+
+Total : 24 files,  10241 codes, 65 comments, 269 blanks, all 10575 lines
+
+[details](details.md)
+
+## Languages
+| language | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| XML | 9 | 5,786 | 63 | 9 | 5,858 |
+| Log | 5 | 3,552 | 0 | 196 | 3,748 |
+| C# | 7 | 504 | 2 | 62 | 568 |
+| JSON | 2 | 398 | 0 | 2 | 400 |
+| Markdown | 1 | 1 | 0 | 0 | 1 |
+
+## Directories
+| path | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| . | 24 | 10,241 | 65 | 269 | 10,575 |
+| Assets | 7 | 504 | 2 | 62 | 568 |
+| Assets\Sctripts | 7 | 504 | 2 | 62 | 568 |
+| Assets\Sctripts\chekers | 5 | 322 | 0 | 43 | 365 |
+| Assets\Sctripts\chekers\board | 1 | 54 | 0 | 9 | 63 |
+| Assets\Sctripts\chekers\move | 1 | 85 | 0 | 14 | 99 |
+| Assets\Sctripts\chekers\movement | 1 | 103 | 0 | 8 | 111 |
+| Assets\Sctripts\chekers\rules | 1 | 63 | 0 | 9 | 72 |
+| Assets\Sctripts\controller | 2 | 182 | 2 | 19 | 203 |
+| Logs | 5 | 3,552 | 0 | 196 | 3,748 |
+| Packages | 2 | 398 | 0 | 2 | 400 |
+
+[details](details.md)
--- Assets/Sctripts/controller/Controller.cs
+++ Assets/Sctripts/controller/Controller.cs
@@ -1,57 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using option;
+using checkers;
+using movement;
+using rules;
+using move;
+
 namespace controller {
-    public struct Checker {
-        public Color color;
-        public Type type;
-    }
-
-    public enum MovementType {
-        Move,
-        Attack
-    }
-
-    public struct Linear {
-        public Vector2Int dir;
-        public int length;
-
-        public static Linear Mk(Vector2Int dir, int length) {
-            return new Linear { dir = dir, length = length };
-        }
-    }
-
-    public struct FixedMovement {
-        public Linear linear;
-        public Vector2Int pos;
-
-        public static FixedMovement Mk(Linear linear, Vector2Int pos) {
-            return new FixedMovement { linear = linear, pos = pos };
-        }
-    }
-
-    public struct CheckerMovement {
-        public FixedMovement movement;
-        public MovementType type;
-
-        public static CheckerMovement Mk(FixedMovement movement, MovementType type) {
-            return new CheckerMovement { movement = movement, type = type };
-        }
-    }
-
-
-    public enum Type {
-        Checker,
-        King
-    }
-
-    public enum Color {
-        White,
-        Black,
-        Count
-    }
-
     enum Action {
         None,
         Select,
@@ -64,9 +21,8 @@ namespace controller {
         public GameObject blackChecker;
         private Option<Checker>[,] board = new Option<Checker>[8, 8];
         private GameObject[,] boardObj = new GameObject[8, 8];
-        private List<Vector2Int> possibleMoves = new List<Vector2Int>();
-        private Vector2Int lastSelectedPos;
-        private Color whoseMove;
+        private List<MoveInfo> moveInfos = new List<MoveInfo>();
+        private rules.Color whoseMove;
         private Action action;
         public GameObject storageHighlightCells;
         public GameObject highlightCell;
@@ -95,37 +51,53 @@ namespace controller {
             switch (action) {
                 case Action.Select:
                     DestroyChildrens(storageHighlightCells.transform);
-                    possibleMoves.Clear();
-                    var checkerMovement = GetCheckersMovement(board, selectedPos);
-                    possibleMoves = GetMoves(board, checkerMovement);
-                    lastSelectedPos = selectedPos;
-                    HighlightCells(possibleMoves);
+                    moveInfos.Clear();
+                    var (checkerMovement, err) = Movement.GetCheckersMovement(
+                        board,
+                        selectedPos,
+                        pieceOpt.Peel()
+                    );
+                    if (err != MovementErrors.None) {
+                        Debug.LogError("CantGetCheckersMovement");
+                        return;
+                    }
+                    var (moves, getMovesErr) = Move.GetMoveInfos(board, checkerMovement);
+                    if (getMovesErr != MoveErrors.None) {
+                        Debug.LogError("CantGetMoves");
+                        return;
+                    }
+                    moveInfos = moves;
+                    HighlightCells(moveInfos);
                     action = Action.Move;
                     break;
                 case Action.Move:
-                    Move(possibleMoves, selectedPos, lastSelectedPos);
+                    var moveInfo = GetMoveInfo(moveInfos, selectedPos);
+                    if (!moveInfo.HasValue) {
+                        action = Action.None;
+                        DestroyChildrens(storageHighlightCells.transform);
+                        moveInfos.Clear();
+                        break;
+                    }
+                    move.Move.CheckerMove(board, moveInfo.Value);
+                    RelocateChecker(moveInfo.Value);
                     DestroyChildrens(storageHighlightCells.transform);
-                    possibleMoves.Clear();
+                    moveInfos.Clear();
                     action = Action.None;
+                    whoseMove = (rules.Color)((int)(whoseMove + 1) % (int)rules.Color.Count);
                     break;
             }
         }
 
-        private void Move(List<Vector2Int> possibleMoves, Vector2Int to, Vector2Int from) {
+        private void RelocateChecker(MoveInfo move) {
+            var from = move.moveDate.from;
+            var to = move.moveDate.to;
             var boardPos = gameObject.transform.position;
-            foreach (var move in possibleMoves) {
-                if (move == to) {
-                    board[to.x, to.y] = Option<Checker>.Some(board[from.x, from.y].Peel());
-                    board[from.x, from.y] = Option<Checker>.None();
-                    boardObj[from.x, from.y].transform.position = new Vector3(
-                    to.x + boardPos.x - cellSize.lossyScale.x * 4 + cellSize.lossyScale.x / 2,
-                    boardPos.y + cellSize.lossyScale.x / 2,
-                    to.y + boardPos.z - cellSize.lossyScale.x * 4 + cellSize.lossyScale.x / 2
-                    );
-                    whoseMove = (Color)((int)(whoseMove + 1) % (int)Color.Count);
-                    break;
-                }
-            }
+            boardObj[from.x, from.y].transform.position = new Vector3(
+                to.x + boardPos.x - cellSize.lossyScale.x * 4 + cellSize.lossyScale.x / 2,
+                boardPos.y + cellSize.lossyScale.x / 2,
+                to.y + boardPos.z - cellSize.lossyScale.x * 4 + cellSize.lossyScale.x / 2
+            );
+            boardObj[to.x, to.y] = boardObj[from.x, from.y];
         }
 
         private void CheckerSpawner(Option<Checker>[,] board) {
@@ -134,7 +106,7 @@ namespace controller {
                     if (board[i, j].IsSome()) {
                         var checker = board[i, j].Peel();
                         GameObject obj;
-                        if (checker.color == Color.White) {
+                        if (checker.color == rules.Color.White) {
                             obj = whiteChecker;
                         } else {
                             obj = blackChecker;
@@ -146,83 +118,6 @@ namespace controller {
             }
         }
 
-        public List<Vector2Int> GetMoves(
-            Option<Checker>[,] board,
-            List<CheckerMovement> checkerMovements
-        ) {
-            var moves = new List<Vector2Int>();
-            foreach (var checkerMovement in checkerMovements) {
-                var movement = checkerMovement.movement;
-                var length = GetLength(board, movement.linear, movement.pos);
-                moves.AddRange(GetMoveCells(movement.linear.dir, movement.pos, length));
-            }
-            return moves;
-        }
-
-        public int GetLength(Option<Checker>[,] board, Linear linear, Vector2Int pos) {
-            int length = 0;
-            var boardSize = new Vector2Int (board.GetLength(1), board.GetLength(0));
-            for (int i = 1; i <= linear.length; i++) {
-                var cell = pos + linear.dir * i;
-                if (!IsOnBoard(boardSize, pos)) {
-                    break;
-                }
-                length ++;
-                if (board[cell.x, cell.y].IsSome()) {
-                    break;
-                }
-            }
-            return length;
-        }
-
-        public List<Vector2Int> GetMoveCells(Vector2Int dir, Vector2Int pos, int length) {
-            var moveCells = new List<Vector2Int>();
-            for (int i = 1; i <= length; i++) {
-                var cell = pos + dir * i;
-                moveCells.Add(cell);
-            }
-            return moveCells;
-        }
-
-        public bool IsOnBoard(Vector2Int boardSize, Vector2Int pos) {
-            if (pos.x < 0 || pos.x > boardSize.x || pos.y < 0 || pos.y > boardSize.y) {
-                return false;
-            }
-            return true;
-        }
-
-        public List<CheckerMovement> GetCheckersMovement(Option<Checker>[,] board, Vector2Int pos) {
-            if (board[pos.x, pos.y].IsNone()) {
-                return null;
-            }
-            var checker = board[pos.x, pos.y].Peel();
-            List<CheckerMovement> checkerMovements = new List<CheckerMovement>();
-            switch (checker.type) {
-                case Type.Checker:
-                    int dir = 1;
-                    checkerMovements.Add(CheckerMovement.Mk(FixedMovement.Mk(Linear.Mk(new Vector2Int(1, 1), 1), pos), MovementType.Attack));
-                    checkerMovements.Add(CheckerMovement.Mk(FixedMovement.Mk(Linear.Mk(new Vector2Int(1, -1), 1), pos), MovementType.Attack));
-                    checkerMovements.Add(CheckerMovement.Mk(FixedMovement.Mk(Linear.Mk(new Vector2Int(-1, 1), 1), pos), MovementType.Attack));
-                    checkerMovements.Add(CheckerMovement.Mk(FixedMovement.Mk(Linear.Mk(new Vector2Int(-1, -1), 1), pos), MovementType.Attack));
-                    if (checker.color == Color.Black) {
-                        dir = -1;
-                    }
-                    checkerMovements.Add(CheckerMovement.Mk(FixedMovement.Mk(Linear.Mk(new Vector2Int(-1, 1) * dir, 1), pos), MovementType.Move));
-                    checkerMovements.Add(CheckerMovement.Mk(FixedMovement.Mk(Linear.Mk(new Vector2Int(-1, -1) * dir, 1), pos), MovementType.Move));
-                    break;
-                case Type.King:
-                    checkerMovements.Add(CheckerMovement.Mk(FixedMovement.Mk(Linear.Mk(new Vector2Int(1, 1), 8), pos), MovementType.Attack));
-                    checkerMovements.Add(CheckerMovement.Mk(FixedMovement.Mk(Linear.Mk(new Vector2Int(1, -1), 8), pos), MovementType.Attack));
-                    checkerMovements.Add(CheckerMovement.Mk(FixedMovement.Mk(Linear.Mk(new Vector2Int(-1, 1), 8), pos), MovementType.Attack));
-                    checkerMovements.Add(CheckerMovement.Mk(FixedMovement.Mk(Linear.Mk(new Vector2Int(-1, -1), 8), pos), MovementType.Attack));
-                    checkerMovements.Add(CheckerMovement.Mk(FixedMovement.Mk(Linear.Mk(new Vector2Int(1, 1), 8), pos), MovementType.Move));
-                    checkerMovements.Add(CheckerMovement.Mk(FixedMovement.Mk(Linear.Mk(new Vector2Int(1, -1), 8), pos), MovementType.Move));
-                    checkerMovements.Add(CheckerMovement.Mk(FixedMovement.Mk(Linear.Mk(new Vector2Int(-1, 1), 8), pos), MovementType.Move));
-                    checkerMovements.Add(CheckerMovement.Mk(FixedMovement.Mk(Linear.Mk(new Vector2Int(-1, -1), 8), pos), MovementType.Move));
-                    break;
-            }
-            return checkerMovements;
-        }
 
         private void DestroyChildrens(Transform parent) {
             foreach (Transform child in parent) {
@@ -245,46 +140,49 @@ namespace controller {
             return Instantiate(gameObject, spawnWorldPos, Quaternion.identity, parentTransform);
         }
 
-        private void HighlightCells(List<Vector2Int> possibleMoves) {
+        private MoveInfo? GetMoveInfo(List<MoveInfo> moveInfos, Vector2Int selectPos) {
+            foreach (var info in moveInfos) {
+                if (info.moveDate.to == selectPos) {
+                    return info;
+                }
+            }
+            return null;
+        }
+
+        private void HighlightCells(List<MoveInfo> possibleMoves) {
             var parentTransform = storageHighlightCells.transform;
             var boardPos = gameObject.transform.position;
             foreach (var pos in possibleMoves) {
-                var spawnWorldPos = new Vector3(
-                    pos.x + boardPos.x - cellSize.lossyScale.x * 4 + cellSize.lossyScale.x / 2,
-                    boardPos.y + cellSize.lossyScale.x / 2,
-                    pos.y + boardPos.z - cellSize.lossyScale.x * 4 + cellSize.lossyScale.x / 2
-                );
-
-                Instantiate(highlightCell, spawnWorldPos, Quaternion.identity, parentTransform);
+                ObjectSpawner(highlightCell, pos.moveDate.to, parentTransform);
             }
         }
 
         private void FillingBoard() {
-            board[0, 0] = Option<Checker>.Some(new Checker { color = Color.Black });
-            board[0, 2] = Option<Checker>.Some(new Checker { color = Color.Black });
-            board[0, 4] = Option<Checker>.Some(new Checker { color = Color.Black });
-            board[0, 6] = Option<Checker>.Some(new Checker { color = Color.Black });
-            board[1, 1] = Option<Checker>.Some(new Checker { color = Color.Black });
-            board[1, 3] = Option<Checker>.Some(new Checker { color = Color.Black });
-            board[1, 5] = Option<Checker>.Some(new Checker { color = Color.Black });
-            board[1, 7] = Option<Checker>.Some(new Checker { color = Color.Black });
-            board[2, 0] = Option<Checker>.Some(new Checker { color = Color.Black });
-            board[2, 2] = Option<Checker>.Some(new Checker { color = Color.Black });
-            board[2, 4] = Option<Checker>.Some(new Checker { color = Color.Black });
-            board[2, 6] = Option<Checker>.Some(new Checker { color = Color.Black });
+            board[0, 0] = Option<Checker>.Some(new Checker { color = rules.Color.Black });
+            board[0, 2] = Option<Checker>.Some(new Checker { color = rules.Color.Black });
+            board[0, 4] = Option<Checker>.Some(new Checker { color = rules.Color.Black });
+            board[0, 6] = Option<Checker>.Some(new Checker { color = rules.Color.Black });
+            board[1, 1] = Option<Checker>.Some(new Checker { color = rules.Color.Black });
+            board[1, 3] = Option<Checker>.Some(new Checker { color = rules.Color.Black });
+            board[1, 5] = Option<Checker>.Some(new Checker { color = rules.Color.Black });
+            board[1, 7] = Option<Checker>.Some(new Checker { color = rules.Color.Black });
+            board[2, 0] = Option<Checker>.Some(new Checker { color = rules.Color.Black });
+            board[2, 2] = Option<Checker>.Some(new Checker { color = rules.Color.Black });
+            board[2, 4] = Option<Checker>.Some(new Checker { color = rules.Color.Black });
+            board[2, 6] = Option<Checker>.Some(new Checker { color = rules.Color.Black });
 
-            board[5, 7] = Option<Checker>.Some(new Checker { color = Color.White });
-            board[5, 5] = Option<Checker>.Some(new Checker { color = Color.White });
-            board[5, 3] = Option<Checker>.Some(new Checker { color = Color.White });
-            board[5, 1] = Option<Checker>.Some(new Checker { color = Color.White });
-            board[6, 6] = Option<Checker>.Some(new Checker { color = Color.White });
-            board[6, 4] = Option<Checker>.Some(new Checker { color = Color.White });
-            board[6, 2] = Option<Checker>.Some(new Checker { color = Color.White });
-            board[6, 0] = Option<Checker>.Some(new Checker { color = Color.White });
-            board[7, 7] = Option<Checker>.Some(new Checker { color = Color.White });
-            board[7, 5] = Option<Checker>.Some(new Checker { color = Color.White });
-            board[7, 3] = Option<Checker>.Some(new Checker { color = Color.White });
-            board[7, 1] = Option<Checker>.Some(new Checker { color = Color.White });
+            board[5, 7] = Option<Checker>.Some(new Checker { color = rules.Color.White });
+            board[5, 5] = Option<Checker>.Some(new Checker { color = rules.Color.White });
+            board[5, 3] = Option<Checker>.Some(new Checker { color = rules.Color.White });
+            board[5, 1] = Option<Checker>.Some(new Checker { color = rules.Color.White });
+            board[6, 6] = Option<Checker>.Some(new Checker { color = rules.Color.White });
+            board[6, 4] = Option<Checker>.Some(new Checker { color = rules.Color.White });
+            board[6, 2] = Option<Checker>.Some(new Checker { color = rules.Color.White });
+            board[6, 0] = Option<Checker>.Some(new Checker { color = rules.Color.White });
+            board[7, 7] = Option<Checker>.Some(new Checker { color = rules.Color.White });
+            board[7, 5] = Option<Checker>.Some(new Checker { color = rules.Color.White });
+            board[7, 3] = Option<Checker>.Some(new Checker { color = rules.Color.White });
+            board[7, 1] = Option<Checker>.Some(new Checker { color = rules.Color.White });
         }
     }
 }
