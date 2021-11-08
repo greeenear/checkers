86e653141fda321a7de0d06105fbf65830868530
yra esepenok <113322greenear@gmail.com>
2021-11-08 18:53:26 +0300

    new checker kind

--- Assets/Scripts/controller/Resources.cs
+++ Assets/Scripts/controller/Resources.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 namespace controller {
     public class Resources : MonoBehaviour {
-        public Transform cellTransform;
-        public Vector2Int boardSize = new Vector2Int(8, 8);
         public GameObject storageHighlightCells;
         public GameObject highlightCell;
         public GameObject whiteChecker;
         public GameObject blackChecker;
+
         public Transform boardTransform;
+        public Transform cellTransform;
+        public Vector2Int boardSize = new Vector2Int(8, 8);
+
         public GameObject gameMenu;
+
+        public Transform boardTransform10x10;
+        public Transform cellTransform10x10;
+
         public readonly List<Vector2Int> directions = new List<Vector2Int> {
             new Vector2Int(1, 1),
             new Vector2Int(-1, 1),
             new Vector2Int(1, -1),
             new Vector2Int(-1, -1)
         };
+
+        public void InitializeBoard(string chKind) {
+            if (chKind == "International") {
+                boardTransform = boardTransform10x10;
+                cellTransform = cellTransform10x10;
+                boardSize = new Vector2Int(10,10);
+            }
+        }
     }
 }
